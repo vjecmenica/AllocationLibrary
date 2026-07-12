e91e5b342c344feed19ecc1833caf15d713ceace
vukasin <vjecmenica2001@gmail.com>
2026-07-12 20:45:06 +0200

    Split allocation project into core and API modules

---
+++ allocation-api/src/main/java/allocation/api/AllocationApiApplication.java
@@ -0,0 +1,12 @@
+package allocation.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllocationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllocationApiApplication.class, args);
+    }
+}
---
+++ allocation-api/src/test/java/allocation/api/AllocationApiApplicationTest.java
@@ -0,0 +1,12 @@
+package allocation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllocationApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
